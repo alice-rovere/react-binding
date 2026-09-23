32e0c6434c2dff7c11d829154db2c19b5a32b544
alice-rovere <arovere1@gmail.com>
2026-09-23 10:03:31 +0200

    esercizio uno completato

--- src/ui/FirstExerciseSection.jsx
+++ src/ui/FirstExerciseSection.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
 export default function FirstExerciseSection() {
-  return null;
+  const [increment, setIncrement] = useState(0);
+  function handleIncrement() {
+    setIncrement(increment + 1);
+  }
+  function handleReset() {
+    setIncrement(0);
+  }
+  return (
+    <div>
+      <p>{increment}</p>
+      <button className="btn btn-primary mb-3" onClick={handleIncrement}>
+        Aumenta il contatore
+      </button>
+      <button className="btn btn-secondary mb-3 ms-2" onClick={handleReset}>
+        Resetta il contatore
+      </button>
+    </div>
+  );
 }
