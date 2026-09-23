33eda517587d0b8a58a9805ab5ee523c9e584589
alice-rovere <arovere1@gmail.com>
2026-09-23 10:17:50 +0200

    esercizio due completato

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,7 +1,7 @@
 export default function ExerciseCard({ titolo, descrizione, children }) {
   return (
     <div className="container card mb-3 w-50">
-      <h2 className="card-title">{titolo}</h2>
+      <h2 className="card-title display-6 ">{titolo}</h2>
       <p className="card-text">{descrizione}</p>
       {children}
     </div>
--- src/ui/SecondExerciseSection.jsx
+++ src/ui/SecondExerciseSection.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export default function SecondExerciseSection() {
-  return null;
+  const [isSuccess, setIsSuccess] = useState(false);
+  function handleIsSuccess() {
+    setIsSuccess(!isSuccess);
+  }
+
+  return (
+    <div>
+      <button
+        className={`btn btn-primary mb-3 ${isSuccess ? "btn-success" : "btn-primary"}`}
+        onClick={handleIsSuccess}
+      >
+        Click me to see the color change
+      </button>
+    </div>
+  );
 }
