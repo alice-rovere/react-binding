a7c140c68d00467d27fbe913a045351a737d62d7
alice-rovere <arovere1@gmail.com>
2026-09-23 10:53:33 +0200

    esercizio quattro completato---si può iterare sopra?

---
+++ src/data/welcomeMessages.js
@@ -0,0 +1,24 @@
+const welcomeMessages = [
+    { id: 1,
+        lingua: 'en',
+        message: 'Welcome to the React Binding Exercises!',
+    },
+    { id: 2,
+        lingua: 'es',
+        message: '¡Bienvenido a los ejercicios de React Binding!',
+    },
+    { id: 3,
+        lingua: 'fr',
+        message: 'Bienvenue aux exercices de liaison React!',
+    },
+    { id: 4,   
+        lingua: 'de',
+        message: 'Willkommen zu den React Binding-Übungen!',
+    },  
+    {
+        id: 5,
+        lingua: 'it',
+        message: 'Benvenuto agli esercizi di React Binding!',
+    }
+]
+export default welcomeMessages;
--- src/ui/FourthExerciseSection.jsx
+++ src/ui/FourthExerciseSection.jsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+import welcomeMessages from "../data/welcomeMessages";
 export default function FourthExerciseSection() {
-  return null;
+  const [selectLanguage, setSelectLanguage] = useState("en");
+  function handleSelectLanguage(lang) {
+    setSelectLanguage(lang);
+  }
+  return (
+    <div>
+      <button
+        onClick={() => handleSelectLanguage("en")}
+        className="btn btn-outline-primary"
+      >
+        English
+      </button>
+      <button
+        onClick={() => handleSelectLanguage("es")}
+        className="btn btn-outline-secondary"
+      >
+        Español
+      </button>
+      <button
+        onClick={() => handleSelectLanguage("fr")}
+        className="btn btn-outline-info"
+      >
+        Français
+      </button>
+      <button
+        onClick={() => handleSelectLanguage("de")}
+        className="btn btn-outline-warning"
+      >
+        Deutsch
+      </button>
+      <button
+        onClick={() => handleSelectLanguage("it")}
+        className="btn btn-outline-dark"
+      >
+        Italiano
+      </button>
+
+      <p>
+        {welcomeMessages.find((msg) => msg.lingua === selectLanguage)?.message}
+      </p>
+    </div>
+  );
 }
