fb237d777f7bfbc9f4905696b1bfaf1953d90e33
alice-rovere <arovere1@gmail.com>
2026-09-25 17:48:04 +0200

    iniziato esercizio 11 - da concludere

--- src/ui/UndicesimoExerciseSection.jsx
+++ src/ui/UndicesimoExerciseSection.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
 export default function UndicesimoExerciseSection() {
-  return <div>UndicesimoExerciseSection</div>;
+  const [grassetto, setGrassetto] = useState(false);
+  const [corsivo, setCorsivo] = useState(false);
+  const [sottolineato, setSottolineato] = useState(false);
+  const [evidenziato, setEvidenziato] = useState(false);
+
+  return (
+    <div>
+      <p className={grassetto === true ? "fw-bold" : ""}>Testo da modificare</p>
+      <input
+        onChange={() => setGrassetto(!grassetto)}
+        value={grassetto}
+        type="checkbox"
+        name="grassetto"
+        id="grassetto"
+      ></input>
+      <label id="grassetto ">grassetto</label>
+      <input
+        onChange={() => setCorsivo(!corsivo)}
+        value={corsivo}
+        type="checkbox"
+        name="corsivo"
+        id="corsivo"
+      />
+      <label id="corsivo">corsivo</label>
+      <input
+        onChange={() => setSottolineato(!sottolineato)}
+        value={sottolineato}
+        type="checkbox"
+        name="sottolineato"
+        id="sottolineato"
+      />
+      <label id="sottolineato">sottolineato</label>
+      <input
+        onChange={() => setEvidenziato(!evidenziato)}
+        value={evidenziato}
+        type="checkbox"
+        name="evidenziato"
+        id="evidenziato"
+      />
+      <label id="evidenziato">evidenziato</label>
+    </div>
+  );
 }
