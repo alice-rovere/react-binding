644900b5e8863de46a0804a6acded096d21105f0
alice-rovere <arovere1@gmail.com>
2026-09-25 19:47:27 +0200

    aggiutna logica esercizio 14

--- src/ui/QuattordicesimoExerciseSection.jsx
+++ src/ui/QuattordicesimoExerciseSection.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function QuattordicesimoExerciseSection() {
-  return <div>QuattordicesimoExerciseSection</div>;
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <textarea
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        name="appunti"
+        id="appunti"
+        maxLength={10}
+      ></textarea>
+      <p>{`Ti mancano ${10 - text.length} caratteri`}</p>
+    </div>
+  );
 }
