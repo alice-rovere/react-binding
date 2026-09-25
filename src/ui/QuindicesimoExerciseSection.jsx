eee319503b5d9f1ae06b402678896b7b580860d3
alice-rovere <arovere1@gmail.com>
2026-09-25 19:58:35 +0200

    aggiunta logica esercizio 15

--- src/ui/QuindicesimoExerciseSection.jsx
+++ src/ui/QuindicesimoExerciseSection.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
 export default function QuindicesimoExerciseSection() {
-  return <div>QuindicesimoExerciseSection</div>;
+  const [text, setText] = useState("");
+  function handleTextLength() {
+    if (text.length < 30) {
+      return "troppo corto";
+    }
+
+    if (text.length > 100) {
+      return "troppo lungo";
+    }
+
+    return "lunghezza perfetta";
+  }
+
+  return (
+    <div>
+      <textarea
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        name="appunti"
+        id="appunti"
+      ></textarea>
+      <p>{handleTextLength()}</p>
+    </div>
+  );
 }
