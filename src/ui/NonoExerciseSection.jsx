e27fdbf9bcf20917d876305f0536aa51253367d9
alice-rovere <arovere1@gmail.com>
2026-09-25 17:12:50 +0200

    aggiunta logica per esercizio 9

--- src/ui/NonoExerciseSection.jsx
+++ src/ui/NonoExerciseSection.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export default function NonoExerciseSection() {
-  return <div>NonoExerciseSection</div>;
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+
+  return (
+    <div>
+      <label className="form-label" htmlFor="nome">
+        Scrivi il tuo nome
+      </label>
+      <input
+        onChange={(e) => setNome(e.target.value)}
+        value={nome}
+        className="form-control text-capitalize"
+        id="nome"
+      ></input>
+      <label className="form-label" htmlFor="cognome">
+        Scrivi il tuo cognome
+      </label>
+      <input
+        onChange={(e) => setCognome(e.target.value)}
+        value={cognome}
+        className="form-control text-capitalize"
+        id="cognome"
+      ></input>
+
+      <p className="text-capitalize">
+        Nome/Cognome: {nome} {cognome}
+      </p>
+    </div>
+  );
 }
