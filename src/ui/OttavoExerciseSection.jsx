147975eb92911f7e145aa4f11311cc06bedaaf50
alice-rovere <arovere1@gmail.com>
2026-09-25 17:02:18 +0200

    aggiunta logica esercizio 8

--- src/ui/OttavoExerciseSection.jsx
+++ src/ui/OttavoExerciseSection.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function OttavoExerciseSection() {
-  return <div>OttavoExerciseSection</div>;
+  const [testo, setTesto] = useState("");
+
+  return (
+    <div>
+      <h1 className="fs-2">{testo}</h1>
+      <label className="form-label">Scrivi per modificare il titolo</label>
+      <input
+        onChange={(e) => setTesto(e.target.value)}
+        value={testo}
+        type="text"
+        className="form-control"
+      />
+    </div>
+  );
 }
