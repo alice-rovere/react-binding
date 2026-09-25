11fc6a4427da98a9644a62295d602678f0cdfa83
alice-rovere <arovere1@gmail.com>
2026-09-25 17:28:03 +0200

    aggiunta logica esercizio 10

--- src/ui/DecimoExerciseSection.jsx
+++ src/ui/DecimoExerciseSection.jsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
 export default function DecimoExerciseSection() {
-  return <div>DecimoExerciseSection</div>;
+  const [haCliccato, setHaCliccato] = useState(false);
+
+  return (
+    <div>
+      <label>Vuoi vedere altro ?</label>
+      <input
+        value={haCliccato}
+        checked={haCliccato === true}
+        type="checkbox"
+        onChange={() => setHaCliccato(!haCliccato)}
+      ></input>
+      <p className={haCliccato === true ? "" : "d-none"}>
+        Hai cliccato che vuoi vedere altro !
+      </p>
+    </div>
+  );
 }
+
+// NON CI CREDOOOO FUNZIONAAAAA BRAVA MEEEEE
