a1df9425395a41b70dee52ba860992004916c365
alice-rovere <arovere1@gmail.com>
2026-09-25 19:40:39 +0200

    aggiunta logica esercizio 13

--- src/ui/TredicesimoExerciseSection.jsx
+++ src/ui/TredicesimoExerciseSection.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export default function TredicesimoExerciseSection() {
-  return <div>TredicesimoExerciseSection</div>;
+  const [valuta, setValuta] = useState("12 euro");
+
+  return (
+    <div>
+      <p>
+        Questo prodotto costa
+        <span> {valuta}</span>
+      </p>
+      <select
+        onChange={(e) => setValuta(e.target.value)}
+        name="valuta"
+        id="valuta"
+      >
+        <option value="12 euro">Euro</option>
+        <option value="13,68 USD">USD</option>
+        <option value="10,32 GBP">GBP</option>
+      </select>
+    </div>
+  );
 }
+// 1 euro = 1.14 usd; 1 euro = 0.86 gbp
